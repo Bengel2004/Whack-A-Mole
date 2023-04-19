11545917b102b760d8e4457bc131fff70c3771fd
Niels <niels.poelder@gmail.com>
2023-04-19 22:55:09 +0200

    🔨 Add object Pooler
    Added a specific object pooler that will allow us to have different moles and other objects to be grabbed.

---
+++ Assets/Scripts/ObjectPooling/NestedObjectPooler.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace WhackAMole.ObjectPooling
+{
+    public class NestedObjectPooler : MonoBehaviour
+    {
+        #region Serialized Fields
+
+        [SerializeField] private List<NestedPool> pool;
+        [SerializeField] private GameObject[] prefabs;
+        [SerializeField] private int size;
+
+        #endregion
+
+        #region Private Fields
+
+        private List<int> currentIndex = new();
+        private int spawnIndex;
+
+        #endregion
+
+        #region Setup
+
+        private void Start()
+        {
+            pool = new List<NestedPool>(4);
+            foreach (GameObject prefab in prefabs)
+            {
+                NestedPool _temp = new NestedPool();
+                pool.Add(_temp);
+            }
+
+            ObjectPool(size);
+        }
+
+        #endregion
+
+        #region Private
+
+        private void ObjectPool(int size)
+        {
+            foreach (GameObject prefab in prefabs)
+            {
+
+                pool[spawnIndex].nesting = new List<GameObject>(size);
+                currentIndex.Add(0); // Adds a new int to start from
+                for (int i = 0; i < size; ++i)
+                {
+                    Spawn();
+                }
+
+                spawnIndex += 1;
+            }
+        }
+
+        private void Spawn()
+        {
+            GameObject obj = Object.Instantiate(prefabs[spawnIndex]);
+            obj.SetActive(false);
+            obj.transform.SetParent(transform);
+            pool[spawnIndex].nesting.Add(obj);
+        }
+
+        #endregion
+
+        #region Public
+
+        public GameObject GetNext(int _value, Vector3 _position, Quaternion _rotation)
+        {
+            GameObject obj = pool[_value].nesting[currentIndex[_value]];
+            currentIndex[_value] = ++currentIndex[_value] % pool[_value].nesting.Count;
+            if (obj != null)
+            {
+                obj.SetActive(true);
+                // rotates obj items according to the pool
+                obj.transform.position = _position;
+                obj.transform.rotation = _rotation;
+            }
+            return obj;
+        }
+
+        #endregion
+    }
+}
---
+++ Assets/Scripts/ObjectPooling/NestedPool.cs
@@ -0,0 +1,8 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class NestedPool
+{
+    public List<GameObject> nesting;
+}
