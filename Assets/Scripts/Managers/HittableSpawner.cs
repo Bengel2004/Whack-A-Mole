8f52ae6c1cd6e7522ddd2002433b09efb6d2ad6e
Niels Poelder <niels.poelder@gmail.com>
2023-04-21 11:43:00 +0200

    🔥 Remove unused code

--- Assets/Scripts/Managers/HittableSpawner.cs
+++ Assets/Scripts/Managers/HittableSpawner.cs
@@ -10,17 +10,6 @@ namespace WhackAMole.Managers
     {
         [SerializeField] private NestedObjectPooler _objectPool;
         [SerializeField] private HoleGenerator _holeGenerator;
-        // Start is called before the first frame update
-        void Start()
-        {
-
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-
-        }
 
         /// <summary>
         /// Spawns a hittable object.
@@ -30,7 +19,6 @@ namespace WhackAMole.Managers
         {
             GameObject tempObject = _objectPool.GetNext(0, _holeGenerator.GetRandomHolePosition(), Quaternion.identity);
             IHittable tempHittable = tempObject.GetComponent<IHittable>();
-            tempHittable.moveAble = tempObject.GetComponent<IMovable>();
             return tempHittable;
         }
     }
