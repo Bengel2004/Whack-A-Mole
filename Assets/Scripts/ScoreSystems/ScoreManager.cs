f3b40802a8bc2e98adfbb3163408e83f8d089a2a
Niels Poelder <niels.poelder@gmail.com>
2023-04-21 17:46:02 +0200

    🔨 Add highscore systems

--- Assets/Scripts/ScoreSystems/ScoreManager.cs
+++ Assets/Scripts/ScoreSystems/ScoreManager.cs
@@ -1,18 +1,74 @@
-using System.Collections;
-using System.Collections.Generic;
-using System;
 using UnityEngine;
+using WhackAMole.Managers;
+using WhackAMole.Interface;
 
-public class ScoreManager : MonoBehaviour
+namespace WhackAMole.ScoreSystems
 {
-    [SerializeField] private TextDisplayer _textDisplayer = default;
-    
-    private float _score = 0;
-
-    public void UpdateScore(float score)
+    public class ScoreManager : MonoBehaviour
     {
-        _score += score;
-        _textDisplayer.DisplayText(_score);
-    }
+        #region Serialized Fields
 
+        [SerializeField] private TextDisplayer _textDisplayer = default;
+
+        #endregion
+
+        #region Private Fields
+
+        private float _score = 0;
+
+        #endregion
+
+        #region Properties
+
+        public float GetScore => _score;
+        public static ScoreManager instance;
+
+        #endregion
+
+        #region Setup
+
+        private void Awake()
+        {
+            if (instance == null)
+            {
+                instance = this;
+            }
+            ResetScore();
+        }
+
+        private void Start()
+        {
+            GameManager.instance.OnStartGame += ResetScore;
+        }
+
+        private void OnDestroy()
+        {
+            GameManager.instance.OnStartGame -= ResetScore;
+        }
+
+        #endregion
+
+        #region Public
+
+        /// <summary>
+        /// Resets the games score.
+        /// </summary>
+        public void ResetScore()
+        {
+            _score = 0;
+            _textDisplayer.DisplayText(_score);
+        }
+
+        /// <summary>
+        /// Updates the games score.
+        /// </summary>
+        /// <param name="score"></param>
+        public void UpdateScore(float score)
+        {
+            _score += score;
+            _textDisplayer.DisplayText(_score);
+        }
+
+        #endregion
+    }
 }
